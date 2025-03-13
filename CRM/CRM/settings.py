f3ef536de737c2d446cf6994bd03527f35654763
Adam Bisharat <abisharat@ucsd.edu>
2025-03-12 18:36:21 -0700

    Fixed port and allowed hosts for Railway

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False  # Set to False for production
 
